6d68a08fd9ff68b99e9b63b18259cca315b275ad
Danyal <danyalbaig18@gmail.com>
2023-07-16 20:52:12 +0500

    Changed POST method in views.py

--- ShopEzy/UserApp/views.py
+++ ShopEzy/UserApp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.db import connection
+from django.contrib import messages
+from django.urls import reverse
 
 # with connection.cursor() as cursor:
 #     cursor.execute("SELECT * FROM products")
@@ -15,6 +17,10 @@ def index(request):
 def signin(request):
     # Render the HTML template signin.html with the data in the context variable
     return render(request, 'UserApp/signin.html')
+
+def admin(request):
+    # Render the HTML template admin.html with the data in the context variable
+    return render(request, 'UserApp/admin.html')
 
 def products(request):
     with connection.cursor() as cursor:
@@ -44,15 +50,27 @@ def contact(request):
     return render(request, 'UserApp/contact.html')
 
 def signin_admin(request):
-    # if request.method == "POST":
-    #     data = request.POST
-    #     if len(data) == 4:
-    #         with connection.cursor() as cursor:
-    #             cursor.execute("SELECT * FROM ADMINISTRATORS WHERE AEMAIL = %s AND APASSWORD = %s AND APASSCODE = %s", [request.POST.get('email'), request.POST.get('password'), request.POST.get('passcode')])
-    #             row = cursor.fetchall()
-    #             print(row)
-            #     if row:
-            #         return render(request, 'UserApp/admin.html')
-            #     else:
-            #         return render(request, 'UserApp/signin_admin.html')
+    if request.method == "POST":
+        data = request.POST
+        if len(data) == 4:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM ADMINISTRATORS WHERE AEMAIL = %s AND APASSWORD = %s AND PASSCODE = %s", [request.POST.get('email'), request.POST.get('password'), request.POST.get('passcode')])
+                row = cursor.fetchone()
+                cursor.close()
+                connection.close()
+                print(row)
+                if row:
+                    return redirect('admin')
+                else:
+                    # Display an error message and redirect to the same page
+                    messages.error(request, 'Invalid credentials.')
+                    return redirect(reverse('signin_admin'))
+        elif len(data) == 7:
+            with connection.cursor() as cursor:
+                cursor.execute("INSERT INTO ADMINISTRATORS(ANAME, AEMAIL, AJOB, PASSCODE, PASSWORD) VALUES(%s, %s, %s, %s, %s)", [request.POST.get('name'), request.POST.get('email'), request.POST.get('job'), request.POST.get('passcode'), request.POST.get('password')])
+                cursor.close()
+                connection.close()
+                return redirect(reverse('signin_admin'))
+        else:
+            return render(request, 'UserApp/signin_admin.html')
     return render(request, 'UserApp/signin_admin.html')
