3461697fae4f8dee261ffce43aa827d7c363c820
Habib Ullah <hu61103@gmail.com>
2023-07-17 01:26:13 -0700

    ADDED PRODUCT QUERIES

---
+++ ShopEzy/UserApp/static/product_all_entry.sql
@@ -0,0 +1,28 @@
+USE StoreDB;
+
+INSERT INTO products (PName, PPrice, PQuantity, PSpecs, PImage)
+VALUES
+	('Samsung Galaxy S21 Ultra', '229999', '10', '../../static/product_description/eletronics-1.txt', '../../static/product_image/eletronics-1.jpg'), 
+	('iPhone 13 Pro Max', '399999', '10', '../../static/product_description/eletronics-2.txt', '../../static/product_image/eletronics-2.jpg'), 
+	('DELL XPS 13', '200000', '10', '../../static/product_description/eletronics-3.txt', '../../static/product_image/eletronics-3.jpg'), 
+	('HP Spectre x360 15', '260000', '10', '../../static/product_description/eletronics-4.txt', '../../static/product_image/eletronics-4.jpg'), 
+	('Samsung QLED Q90R', '250000', '10', '../../static/product_description/eletronics-5.txt', '../../static/product_image/eletronics-5.jpg'), 
+	('LG OLED CX Series', '200000', '10', '../../static/product_description/eletronics-6.txt', '../../static/product_image/eletronics-6.jpg'), 
+	('Apple Watch Series 7', '200000', '10', '../../static/product_description/eletronics-7.txt', '../../static/product_image/eletronics-7.jpg'), 
+	('Samsung Galaxy Watch 4', '70000', '10', '../../static/product_description/eletronics-8.txt', '../../static/product_image/eletronics-8.jpg'),
+    ('Red Shirt', 290, 100, '../../static/product_description/garments-1.txt', '../../static/product_image/garments-1.jpg'),
+    ('Blue Jeans', 399, 150, '../../static/product_description/garments-2.txt', '../../static/product_image/garments-2.jpg'),
+    ('Frock', 499, 80, '../../static/product_description/garments-3.txt', '../../static/product_image/garments-3.jpg'),
+    ('Orange T-Shirt', 199, 200, '../../static/product_description/garments-4.txt', '../../static/product_image/garments-4.jpg'),
+    ('Blue Skirt', 349, 120, '../../static/product_description/garments-5.txt', '../../static/product_image/garments-5.jpg'),
+    ('Brown Pant', 449, 100, '../../static/product_description/garments-6.txt', '../../static/product_image/garments-6.jpg'),
+    ('Red Sweater', 399, 80, '../../static/product_description/garments-7.txt', '../../static/product_image/garments-7.jpg'),
+    ('Rain Coat', 799, 50, '../../static/product_description/garments-8.txt', '../../static/product_image/garments-8.jpg'),
+    ('Rice Bag 10kg', 49, 500, '../../static/product_description/groceries-1.txt', '../../static/product_image/groceries-1.jpg'),
+    ('3 Packs Bread', 29, 300, '../../static/product_description/groceries-2.txt', '../../static/product_image/groceries-2.jpg'),
+    ('Milk Box', 39, 200, '../../static/product_description/groceries-3.txt', '../../static/product_image/groceries-3.jpg'),
+    ('12 Eggs', 29, 400, '../../static/product_description/groceries-4.txt', '../../static/product_image/groceries-4.jpg'),
+    ('Broast Chicken', 999, 100, '../../static/product_description/groceries-5.txt', '../../static/product_image/groceries-5.jpg'),
+    ('12 Apples Pack', 199, 150, '../../static/product_description/groceries-6.txt', '../../static/product_image/groceries-6.jpg'),
+    ('12 Bananas Pack', 99, 250, '../../static/product_description/groceries-7.txt', '../../static/product_image/groceries-7.jpg'),
+    ('Cereal Box', 349, 120, '../../static/product_description/groceries-8.txt', '../../static/product_image/groceries-8.jpg');
